50bfb7d1fc699270afcd7843c7e1116bb01cd49b
Deepanshu Tiwari <ddepu11@gmail.com>
2021-02-26 13:40:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is up and running on http://${HOSTNAME}:${PORT}`);
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Articles" });
+});
